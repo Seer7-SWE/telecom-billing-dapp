1f2c2fc8ab1d4380a6b3c5b5aab4ee3d5d4324ac
Seer7-SWE <seeralan006@gmail.com>
2025-09-05 13:47:43 +0530

    Update api.mjs

--- netlify/functions/api.mjs
+++ netlify/functions/api.mjs
@@ -1,15 +1,27 @@
+// import serverless from "serverless-http";
+// import app, { initBackend } from "../../backend/app.js";
+
+
+
+// // Ensure DB etc. initialized once
+// await initBackend();
+
+// export async function handler(event, context) {
+//   return {
+//     statusCode: 200,
+//     body: JSON.stringify({ message: 'OK' }),
+//   };
+// }
+
 import serverless from "serverless-http";
 import app, { initBackend } from "../../backend/app.js";
 
-
-
-// Ensure DB etc. initialized once
-await initBackend();
+let handler;
 
 export async function handler(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'OK' }),
-  };
+  if (!handler) {
+    await initBackend();
+    handler = serverless(app);
+  }
+  return handler(event, context);
 }
-

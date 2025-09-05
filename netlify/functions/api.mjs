d486a5d7470d9a95b16d3e71cfcf637d8298bd5b
Seer7-SWE <seeralan006@gmail.com>
2025-09-05 13:05:41 +0530

    Update api.mjs

--- netlify/functions/api.mjs
+++ netlify/functions/api.mjs
@@ -2,22 +2,14 @@ import serverless from "serverless-http";
 import app, { initBackend } from "../../backend/app.js";
 
 
-let handler;
 
-export const handler = async (event, context) => {
-  if (!handler) {
-    await initBackend(); // connect Mongo once
-    handler = serverless(app);
-  }
-  return handler(event, context);
-};
 // Ensure DB etc. initialized once
-// await initBackend();
+await initBackend();
 
-// export async function handler(event, context) {
-//   return {
-//     statusCode: 200,
-//     body: JSON.stringify({ message: 'OK' }),
-//   };
-// }
+export async function handler(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'OK' }),
+  };
+}
 
